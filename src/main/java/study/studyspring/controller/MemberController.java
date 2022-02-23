5b85cded245886eae1beba4f10c75fe2ace73995
guddndhk <guddndhk@naver.com>
2022-02-24 02:33:08 +0900

    22/02/24 spring study and review. don't stop keep going noob developer! 컴포넌트 스캔 방식이 아닌 자바코드로(SpringConfig) 로 작성한 이유는 아직 DB 가 확정 되지 않았기에 확정후 간단히 연결하기위함..

---
+++ src/main/java/study/studyspring/controller/MemberController.java
@@ -0,0 +1,17 @@
+package study.studyspring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import study.studyspring.service.MemberService;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    //생성자
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
