9dd4f186bb2f864352c67c348f90960edfd0b920
guddndhk <guddndhk@naver.com>
2022-02-25 04:40:14 +0900

    22/02/25 spring study and review. don't stop keep going noob developer! 메서드 시간체크 aop 적용완료. 전반적인 스프링 기초 복습 완료. 내일부터 좀더 딥하게 학습예정.

--- src/main/java/study/studyspring/SpringConfig.java
+++ src/main/java/study/studyspring/SpringConfig.java
@@ -3,8 +3,11 @@ package study.studyspring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import study.studyspring.aop.TimeTraceAop;
 import study.studyspring.repository.*;
 import study.studyspring.service.MemberService;
+
+import java.net.PortUnreachableException;
 
 @Configuration
 public class SpringConfig {
@@ -35,6 +38,12 @@ public class SpringConfig {
 
         return new MemberService(memberRepository);
     }
+
+    //이처럼 사용하거나 해당 aop 에서 Component 를 사용하여도됨
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
