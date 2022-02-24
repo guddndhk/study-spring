ee63212a41b8439dfeb9e1d4cf39f5eb2dda1de5
guddndhk <guddndhk@naver.com>
2022-02-25 03:15:13 +0900

    22/02/24 spring study and review. don't stop keep going noob developer! Jdbc Template 적용완료.

--- src/main/java/study/studyspring/SpringConfig.java
+++ src/main/java/study/studyspring/SpringConfig.java
@@ -1,22 +1,37 @@
 package study.studyspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import study.studyspring.repository.JdbcMemberRepository;
+import study.studyspring.repository.JdbcTemplateMemberRepository;
 import study.studyspring.repository.MemberRepository;
 import study.studyspring.repository.MemoryMemberRepository;
 import study.studyspring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
 
